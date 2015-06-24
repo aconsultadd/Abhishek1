55d294d56938d324faaab7483e1699d6cd7f6d29
abhishek choubey <abhishek.choubey@consultadd.com>
2015-06-24 11:00:48 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Abhishek::Application.config.secret_token = '3ac59624bdbf59f4b68439f4c2abd56059b2be8331ec911d125880cf211e80f8cf699ff55bf0ff283b575695098d96779265d95aa35b5b7522457bfae4d6bb32'
