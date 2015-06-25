718ec047e1f166806dcdf831951da3c3b366d406
abhishek choubey <abhishek.choubey@consultadd.com>
2015-06-25 11:02:48 +0530

    Created Home,About us and services page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Abhishek::Application.routes.draw do
+  root :to => 'pages#home'
+  get 'about'=> 'pages#about'
+  get 'services'=> 'pages#services'
+   #get "pages/about"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +53,7 @@ Abhishek::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   # root :"home" => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
